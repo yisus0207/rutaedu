1c8e0d8fa20394eed94d0004939b386aa1742baa
Jesus David Salas <jsalas@ises.com.co>
2026-06-10 23:24:41 -0500

    metrcas de visualizaciones

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react";
 
 import type { Viewport } from "next";
 
@@ -44,6 +45,7 @@ export default function RootLayout({
     >
       <body className="min-h-full flex flex-col bg-background text-foreground" suppressHydrationWarning>
         {children}
+        <Analytics />
       </body>
     </html>
   );
